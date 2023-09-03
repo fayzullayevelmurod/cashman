435b3d867df1a7b543d1f6d1ff488ee71ff052e6
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2023-09-04 01:10:49 +0500

    home qismi deyarli tayyor

--- js/app.js
+++ js/app.js
@@ -2,9 +2,12 @@
 // icon.onclick = () => {
 // 	icon.classList.toggle("filled");
 // };
-const heartButtons = document.querySelectorAll(".add-to-favorite");
-heartButtons.forEach((button) => {
-	button.addEventListener("click", () => {
-		button.classList.toggle("clicked");
-	});
-});
+// const heartButtons = document.querySelectorAll(".add-to-favorite");
+// heartButtons.forEach((button) => {
+// 	button.addEventListener("click", () => {
+// 		button.classList.toggle("clicked");
+// 	});
+// });
+$(document).ready(function(){
+	$('.select2').select2()
+})
