b58a9049fbfeed496c85f28e5fce3ea4670bb21f
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2023-10-16 12:17:43 +0500

    modal

--- js/app.js
+++ js/app.js
@@ -99,5 +99,14 @@ $(document).ready(function(){
 		$('.provider_search').toggleClass('active');
 	})
 	
+	$(".show-more").click(function () {
+        if($(".text").hasClass("show-more-height")) {
+            $(this).text("(Show Less)");
+        } else {
+            $(this).text("(Show More)");
+        }
+
+        $(".text").toggleClass("show-more-height");
+    });
 	
 });
